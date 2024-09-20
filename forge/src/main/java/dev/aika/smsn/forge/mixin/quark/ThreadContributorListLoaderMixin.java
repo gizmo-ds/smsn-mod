6c9ed19d0af1e9594c567eef29a72e162e259bf8
Gizmo <me@aika.dev>
2024-09-20 19:17:12 +0800

    1.19.2

--- forge/src/main/java/dev/aika/smsn/forge/mixin/quark/ThreadContributorListLoaderMixin.java
+++ forge/src/main/java/dev/aika/smsn/forge/mixin/quark/ThreadContributorListLoaderMixin.java
@@ -5,7 +5,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(targets = "org.violetmoon.quark.base.handler.ContributorRewardHandler$ThreadContributorListLoader", remap = false)
+@Mixin(targets = "vazkii.quark.base.handler.ContributorRewardHandler$ThreadContributorListLoader", remap = false)
 public class ThreadContributorListLoaderMixin {
     @Inject(method = "run", at = @At("HEAD"), cancellable = true, remap = false)
     private void onRun(CallbackInfo ci) {
