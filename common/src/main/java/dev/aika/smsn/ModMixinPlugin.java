333e81c9eafa6ea6082d04c3cd3d219e7a200ba5
Gizmo <me@aika.dev>
2024-11-22 18:09:50 +0800

    fix: update The Aether Mixin checker

--- common/src/main/java/dev/aika/smsn/ModMixinPlugin.java
+++ common/src/main/java/dev/aika/smsn/ModMixinPlugin.java
@@ -44,7 +44,7 @@ public class ModMixinPlugin implements IMixinConfigPlugin {
             case "com.teamabnormals.blueprint.client.RewardHandler":
                 return SMSNPlatform.isModLoaded("blueprint");
             case "com.aetherteam.nitrogen.api.users.UserData$Server":
-                return SMSNPlatform.isModLoaded("aether");
+                return SMSNPlatform.isModLoaded("nitrogen");
             default:
                 SMSN.LOGGER.warn("Unknown mixin target class: {}", targetClassName);
                 return true;
