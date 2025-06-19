bd3b0f7c469a275d26a34db04f9758250f521702
Gizmo <me@aika.dev>
2025-06-20 07:27:13 +0800

    chore: fix fix-supplementaries

--- scripts/fix-supplementaries.ps1
+++ scripts/fix-supplementaries.ps1
@@ -1,3 +1,3 @@
-New-Item -Name "neoforge/mods" -ItemType "Directory"
+New-Item -Force -Name "neoforge/mods" -ItemType "Directory"
 Invoke-WebRequest -Uri "https://api.modrinth.com/maven/maven/modrinth/supplementaries/8lr6B9o3/supplementaries-8lr6B9o3.jar" -OutFile "neoforge/mods/supplementaries-3.1.1-neoforge.jar"
 zip -d neoforge/mods/supplementaries-3.1.1-neoforge.jar architectury.common.json
