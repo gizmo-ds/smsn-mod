22858a31cdccb0684902c872035765554667358c
Gizmo <me@aika.dev>
2025-08-07 06:41:29 +0800

    fix: Alex's Mods WebHelper Mixins Support BackupFile

--- forge/src/main/java/dev/aika/smsn/forge/mixin/alex/CitadelWebHelperMixin.java
+++ forge/src/main/java/dev/aika/smsn/forge/mixin/alex/CitadelWebHelperMixin.java
@@ -1,5 +1,6 @@
 package dev.aika.smsn.forge.mixin.alex;
 
+import com.github.alexthe666.citadel.web.WebHelper;
 import dev.aika.smsn.SMSN;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -7,11 +8,19 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
 @Mixin(value = com.github.alexthe666.citadel.web.WebHelper.class, remap = false)
 public class CitadelWebHelperMixin {
     @Inject(method = "getURLContents", at = @At(value = "INVOKE", target = "Ljava/net/URL;openConnection()Ljava/net/URLConnection;"), cancellable = true, remap = false)
     private static void onGetURLContents(String urlString, String backupFileLoc, CallbackInfoReturnable<BufferedReader> cir) {
-        if (!SMSN.CONFIG.isAlexModsContributorCheck()) cir.setReturnValue(null);
+        if (SMSN.CONFIG.isAlexModsContributorCheck()) return;
+        BufferedReader result = new BufferedReader(new InputStreamReader(
+                Objects.requireNonNull(WebHelper.class.getClassLoader().getResourceAsStream(backupFileLoc)),
+                StandardCharsets.UTF_8
+        ));
+        cir.setReturnValue(result);
     }
 }
