9638265cf337d0bc15c3663a8184a6156f793efc
Gizmo <me@aika.dev>
2025-08-10 07:09:21 +0800

    chore: update MixinInfo.getMixinClassNames

--- neoforge/src/main/java/dev/aika/smsn/neoforge/mixin/MixinInfo.java
+++ neoforge/src/main/java/dev/aika/smsn/neoforge/mixin/MixinInfo.java
@@ -1,48 +1,34 @@
 package dev.aika.smsn.neoforge.mixin;
 
-import java.util.HashMap;
+import dev.aika.smsn.utils.MapUtils;
+
 import java.util.Map;
 
 public class MixinInfo {
     @SuppressWarnings("SpellCheckingInspection")
     public static Map<String, String> getMixinClassNames() {
-        return new HashMap<>() {{
-            put("dev.aika.smsn.neoforge.mixin.blueprint.RewardHandlerMixin", "blueprint");
-
-            put("dev.aika.smsn.neoforge.mixin.supplementaries.CreditsMixin", "supplementaries");
-
-            put("dev.aika.smsn.neoforge.mixin.petrolpark.BadgeHandlerMixin", "petrolpark");
-
-            put("dev.aika.smsn.neoforge.mixin.immersive_engineering.ImmersiveEngineeringMixin", "immersiveengineering");
-
-            put("dev.aika.smsn.neoforge.mixin.ipn.IPNInfoManagerMixin", "inventoryprofilesnext");
-
-            put("dev.aika.smsn.neoforge.mixin.bagus_lib.TierHelperMixin", "bagus_lib");
-
-            put("dev.aika.smsn.neoforge.mixin.aetherteam.UserData$ServerMixin", "nitrogen");
-
-            put("dev.aika.smsn.neoforge.mixin.placebo.TrailsManagerMixin", "placebo");
-            put("dev.aika.smsn.neoforge.mixin.placebo.WingsManagerMixin", "placebo");
-
-            put("dev.aika.smsn.neoforge.mixin.immersivecaves.DiscordInviteOnJoinProcedureMixin", "immersivecaves");
-
-            put("dev.aika.smsn.neoforge.mixin.iris.UpdateCheckerMixin", "iris");
-
-            put("dev.aika.smsn.neoforge.mixin.actuallyadditions.ThreadSpecialFetcherMixin", "actuallyadditions");
-
-            put("dev.aika.smsn.neoforge.mixin.exposure.GildedMixin", "exposure");
-            put("dev.aika.smsn.neoforge.mixin.exposure.PatreonMixin", "exposure");
-
-            put("dev.aika.smsn.neoforge.mixin.titanium.RewardMixin", "titanium");
-
-            put("dev.aika.smsn.neoforge.mixin.ribbits.SupportersJSONMixin", "ribbits");
-
-            put("dev.aika.smsn.neoforge.mixin.xaero.minimap.InternetMixin", "xaerominimap");
-            put("dev.aika.smsn.neoforge.mixin.xaero.minimap.PatreonMixin", "xaerominimap");
-            put("dev.aika.smsn.neoforge.mixin.xaero.world_map.InternetMixin", "xaeroworldmap");
-            put("dev.aika.smsn.neoforge.mixin.xaero.world_map.PatreonMixin", "xaeroworldmap");
-
-            put("dev.aika.smsn.neoforge.mixin.imb11.mru.APIMixin", "mru");
-        }};
+        return MapUtils.createMap(
+                "dev.aika.smsn.neoforge.mixin.actuallyadditions.ThreadSpecialFetcherMixin", "actuallyadditions",
+                "dev.aika.smsn.neoforge.mixin.aetherteam.UserData$ServerMixin", "nitrogen",
+                "dev.aika.smsn.neoforge.mixin.bagus_lib.TierHelperMixin", "bagus_lib",
+                "dev.aika.smsn.neoforge.mixin.blueprint.RewardHandlerMixin", "blueprint",
+                "dev.aika.smsn.neoforge.mixin.exposure.GildedMixin", "exposure",
+                "dev.aika.smsn.neoforge.mixin.exposure.PatreonMixin", "exposure",
+                "dev.aika.smsn.neoforge.mixin.imb11.mru.APIMixin", "mru",
+                "dev.aika.smsn.neoforge.mixin.immersive_engineering.ImmersiveEngineeringMixin", "immersiveengineering",
+                "dev.aika.smsn.neoforge.mixin.immersivecaves.DiscordInviteOnJoinProcedureMixin", "immersivecaves",
+                "dev.aika.smsn.neoforge.mixin.ipn.IPNInfoManagerMixin", "inventoryprofilesnext",
+                "dev.aika.smsn.neoforge.mixin.iris.UpdateCheckerMixin", "iris",
+                "dev.aika.smsn.neoforge.mixin.petrolpark.BadgeHandlerMixin", "petrolpark",
+                "dev.aika.smsn.neoforge.mixin.placebo.TrailsManagerMixin", "placebo",
+                "dev.aika.smsn.neoforge.mixin.placebo.WingsManagerMixin", "placebo",
+                "dev.aika.smsn.neoforge.mixin.ribbits.SupportersJSONMixin", "ribbits",
+                "dev.aika.smsn.neoforge.mixin.supplementaries.CreditsMixin", "supplementaries",
+                "dev.aika.smsn.neoforge.mixin.titanium.RewardMixin", "titanium",
+                "dev.aika.smsn.neoforge.mixin.xaero.minimap.InternetMixin", "xaerominimap",
+                "dev.aika.smsn.neoforge.mixin.xaero.minimap.PatreonMixin", "xaerominimap",
+                "dev.aika.smsn.neoforge.mixin.xaero.world_map.InternetMixin", "xaeroworldmap",
+                "dev.aika.smsn.neoforge.mixin.xaero.world_map.PatreonMixin", "xaeroworldmap"
+        );
     }
 }
