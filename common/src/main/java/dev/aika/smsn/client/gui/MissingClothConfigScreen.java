55f7a1c622cd9b72aeead8611b8034405b4295bf
Gizmo <me@aika.dev>
2025-08-10 12:17:01 +0800

    chore: update MissingClothConfigScreen

--- common/src/main/java/dev/aika/smsn/client/gui/MissingClothConfigScreen.java
+++ common/src/main/java/dev/aika/smsn/client/gui/MissingClothConfigScreen.java
@@ -5,16 +5,21 @@ import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.MultiLineLabel;
+import net.minecraft.client.gui.screens.ConfirmLinkScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.CommonComponents;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.TranslatableComponent;
 
 public class MissingClothConfigScreen extends Screen {
     private final static String CLOTH_CONFIG_MODRINTH = "https://modrinth.com/mod/9s6osm5g";
     private final static String CLOTH_CONFIG_CURSEFORGE = "https://www.curseforge.com/minecraft/mc-mods/cloth-config";
 
+    private final Component message = new TranslatableComponent("gui.smsn.missing_cloth_config.message");
+    private final Component curseforgeButton = new TranslatableComponent("gui.smsn.missing_cloth_config.curseforge_download");
+    private final Component modrinthButton = new TranslatableComponent("gui.smsn.missing_cloth_config.modrinth_download");
+
     private final Screen parent;
-    private final TranslatableComponent messageComponent = new TranslatableComponent("gui.smsn.missing_cloth_config.message");
 
     public MissingClothConfigScreen(Screen parent) {
         super(new TranslatableComponent("gui.smsn.missing_cloth_config.title"));
@@ -27,25 +32,34 @@ public class MissingClothConfigScreen extends Screen {
 
         var posX = (this.width - 200) / 2;
         var posY = this.height / 2 - 20;
-        posY += this.font.wordWrapHeight(messageComponent.getString(), 300);
-        this.addRenderableWidget(new Button(posX, posY, 100, 20,
-                new TranslatableComponent("gui.smsn.missing_cloth_config.modrinth_download"),
-                b -> Util.getPlatform().openUri(CLOTH_CONFIG_MODRINTH)));
-        this.addRenderableWidget(new Button(posX + 100, posY, 100, 20,
-                new TranslatableComponent("gui.smsn.missing_cloth_config.curseforge_download"),
-                b -> Util.getPlatform().openUri(CLOTH_CONFIG_CURSEFORGE)));
-        posY += 25;
-        this.addRenderableWidget(new Button(posX, posY, 200, 20,
-                CommonComponents.GUI_BACK,
-                (b) -> Minecraft.getInstance().setScreen(this.parent)));
+
+        posY += this.font.wordWrapHeight(message.getString(), 300);
+
+        addRenderableWidget(new Button(posX, posY, 100, 20, curseforgeButton, openLink(CLOTH_CONFIG_CURSEFORGE)));
+        addRenderableWidget(new Button(posX + 100, posY, 100, 20, modrinthButton, openLink(CLOTH_CONFIG_MODRINTH)));
+        addRenderableWidget(new Button(posX, posY + 25, 200, 20, CommonComponents.GUI_BACK, this::onBack));
+    }
+
+    private void onBack(Button ignoredButton) {
+        Minecraft.getInstance().setScreen(parent);
+    }
+
+    private Button.OnPress openLink(String link) {
+        return b -> {
+            if (minecraft == null) return;
+            minecraft.setScreen(new ConfirmLinkScreen((v) -> {
+                if (v) Util.getPlatform().openUri(link);
+                minecraft.setScreen(this);
+            }, link, true));
+        };
     }
 
     @Override
     public void render(PoseStack poseStack, int mouseX, int mouseY, float partialTick) {
-        this.renderBackground(poseStack);
-        MultiLineLabel.create(this.font, new TranslatableComponent("gui.smsn.missing_cloth_config.title"), 300)
+        renderBackground(poseStack);
+        MultiLineLabel.create(this.font, this.title, 300)
                 .renderCentered(poseStack, this.width / 2, this.height / 2 - 45);
-        MultiLineLabel.create(this.font, messageComponent, 300)
+        MultiLineLabel.create(this.font, message, 300)
                 .renderCentered(poseStack, this.width / 2, this.height / 2 - 30);
         super.render(poseStack, mouseX, mouseY, partialTick);
     }
